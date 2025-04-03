923ca23fe5d21baa6166d7d1c012411638a98eb0
tedertree <tendertree@gmail.com>
2025-04-03 11:21:15 +0900

    feat: organize files

---
+++ packages/ui/base/.storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import { join, dirname } from "path";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+/**
+ * Synchronously resolve the absolute path of a package.
+ */
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions')
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {}
+  }
+};
+
+export default config;
